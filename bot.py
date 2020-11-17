0c92cccf26b7fe93034209de73b4e553bc2b4e7b
Rman228 <62659707+Rman228@users.noreply.github.com>
2020-11-17 13:40:16 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -71,7 +71,7 @@ def firstmenu(message):
         keyboard.row(
             telebot.types.InlineKeyboardButton('Отправить сообщение мамонтам', callback_data='sentmamont')
         )
-    bot.send_photo(message.chat.id, 'https://gifyu.com/image/i4K5', reply_markup=keyboard)
+    bot.send_photo(message.chat.id, 'https://ru.files.fm/u/m4hzwmvwk#/view/s33p6xjs4', reply_markup=keyboard)
 
 
 def secondmenu(message):
